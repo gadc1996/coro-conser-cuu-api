edb151ca6b20d660e78979d7f0a238917235dead
gdelgado <gdelgado@controlla.com.mx>
2023-12-06 18:55:04 +0000

    add auth

--- src/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app import views
-
-
-urlpatterns = [
-    path('', views.api_root , name='root'),
-]
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path, include
+from django.urls import path
+from core.views import api_root
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', include('app.urls')),
+    path("", api_root),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
