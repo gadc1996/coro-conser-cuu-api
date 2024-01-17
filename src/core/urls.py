62c453838221e9179cd2a20a04e3e87dbaeaaf1b
Alex Delgado <gadc1996@gmail.com>
2024-01-17 21:55:58 +0000

    Refactor URL patterns in core/urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import api_root
+
 urlpatterns = [
-    path('', api_root),
-    path('admin/', admin.site.urls),
+    path("", api_root),
+    path("admin/", admin.site.urls),
 ]
