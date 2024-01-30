8d8063b60428154d0fc40add05cf2e91be6381a4
Alex Delgado <gadc1996@gmail.com>
2024-01-30 21:49:18 +0000

    Add runserver, migrate, and collectstatic functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 import subprocess
 
 
-if __name__ == "__main__":
+def runserver():
     subprocess.run(["poetry", "run", "gunicorn", "--bind", "0.0.0.0:8080", "core.wsgi:application"], check=True)
+
+def migrate():
+    subprocess.run(["poetry", "run", "python", "manage.py", "migrate"], check=True)
+    
+def collectstatic():
+    subprocess.run(["poetry", "run", "python", "manage.py", "collectstatic", "--noinput"], check=True)
+    
+if __name__ == "__main__":
+    migrate()
+    collectstatic()
+    runserver()
     
